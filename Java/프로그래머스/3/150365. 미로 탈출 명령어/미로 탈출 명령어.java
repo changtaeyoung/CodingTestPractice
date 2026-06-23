6d96f3c277bbe2123b45d7abb43b1a36553d169a
tae0 <83691299+changtaeyoung@users.noreply.github.com>
2026-06-23 13:35:32 +0900

    [level 3] Title: 미로 탈출 명령어, Time: 2.11 ms, Memory: 79 MB -BaekjoonHub

--- Java/프로그래머스/3/150365. 미로 탈출 명령어/미로 탈출 명령어.java
+++ Java/프로그래머스/3/150365. 미로 탈출 명령어/미로 탈출 명령어.java
@@ -1,27 +1,43 @@
+import java.util.*;
+
 class Solution {
-    static int[] dx = {1, 0, 0, -1};
+    
+    static int[] dx = {1, 0, 0, -1}; // d, l, r, u 순서
     static int[] dy = {0, -1, 1, 0};
-    static char[] str;
     static int depth;
-
-    static boolean dfs(int ex, int ey, int sx, int sy, int tx, int ty, int dist) {
-
+    static char[] str;
+    
+    static boolean dfs (int ex, int ey, int sx, int sy, int tx, int ty, int dist) {
+        
         if (depth == dist) {
             return sx == tx && sy == ty;
         }
-
-        int manhattan = Math.abs(sx - tx) + Math.abs(sy - ty);
+        
         int left = dist - depth;
-
-        if (manhattan > left) return false;
-        if ((left - manhattan) % 2 != 0) return false;  // 홀짝 가지치기
-
+        if (Math.abs(sx - tx) + Math.abs(sy - ty) > left) {
+            return false;
+        }
+        if ((left - (Math.abs(sx - tx) + Math.abs(sy - ty))) % 2 != 0) {
+            return false;
+        }
+        
         for (int i = 0; i < 4; i++) {
             int nx = sx + dx[i];
             int ny = sy + dy[i];
-
+            
             if (nx >= 1 && nx <= ex && ny >= 1 && ny <= ey) {
-                str[depth] = i == 0 ? 'd' : i == 1 ? 'l' : i == 2 ? 'r' : 'u';
+                if (i == 0) {
+                    str[depth] = 'd';
+                }
+                else if (i == 1) {
+                    str[depth] = 'l';
+                }
+                else if (i == 2) {
+                    str[depth] = 'r';
+                }
+                else {
+                    str[depth] = 'u';
+                }
                 depth++;
                 if (dfs(ex, ey, nx, ny, tx, ty, dist)) return true;
                 depth--;
@@ -29,16 +45,22 @@ class Solution {
         }
         return false;
     }
-
+    
+    
     public String solution(int n, int m, int x, int y, int r, int c, int k) {
-        str = new char[k];
         depth = 0;
-
-        int manhattan = Math.abs(x - r) + Math.abs(y - c);
-        if (manhattan > k || (k - manhattan) % 2 != 0) {
+        str = new char[k];
+        
+        if (Math.abs(x - r) + Math.abs(y - c) > k) {
             return "impossible";
         }
-
-        return dfs(n, m, x, y, r, c, k) ? String.valueOf(str) : "impossible";
+        
+        if (dfs(n, m, x, y, r, c, k)) {
+            return String.valueOf(str);
+        }
+        else {
+            return "impossible";
+        }
+        
     }
 }
