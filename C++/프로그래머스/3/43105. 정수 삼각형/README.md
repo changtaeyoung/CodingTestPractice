9ff99ba2753ccb47b2d1f4cdaf6f8e17d9bcfa38
tae0 <83691299+changtaeyoung@users.noreply.github.com>
2026-09-11 14:16:31 +0900

    [level 3] Title: 정수 삼각형, Time: 0.57 ms, Memory: 9.32 MB -BaekjoonHub

--- C++/프로그래머스/3/43105. 정수 삼각형/README.md
+++ C++/프로그래머스/3/43105. 정수 삼각형/README.md
@@ -1,10 +1,10 @@
 # [level 3] 정수 삼각형 - 43105 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43105) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43105?language=cpp) 
 
 ### 성능 요약
 
-메모리: 7.28 MB, 시간: 0.51 ms
+메모리: 9.32 MB, 시간: 0.57 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 05월 14일 14:40:16
+2026년 09월 11일 14:16:27
 
 ### 문제 설명
 
--- C++/프로그래머스/3/43105. 정수 삼각형/정수 삼각형.cpp
+++ C++/프로그래머스/3/43105. 정수 삼각형/정수 삼각형.cpp
@@ -1,20 +1,23 @@
 #include <string>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
-int dp[501][501];
+/*
+    탑다운 방식, 바텀업 방식이 존재하나, 가장 무난한 탑다운 방식을 적용해보겠음
+*/
 
 int solution(vector<vector<int>> triangle) {
-    int answer = 0;
-    
+    int dp[501][501], n = triangle.size(), ans = 0;
     dp[0][0] = triangle[0][0];
-    for (int i = 1; i < triangle.size(); i++) {
-        for (int j = 0; j <= i; j++) {
+    
+    for (int i = 1; i < n; i++) {
+        for (int j = 0; j < triangle[i].size(); j++) {
             if (j == 0) {
                 dp[i][j] = dp[i - 1][j] + triangle[i][j];
             }
-            else if (j == i) {
+            else if (j == triangle[i].size() - 1) {
                 dp[i][j] = dp[i - 1][j - 1] + triangle[i][j];
             }
             else {
@@ -23,10 +26,11 @@ int solution(vector<vector<int>> triangle) {
         }
     }
     
-    for (int i = 0; i < triangle.size(); i++) {
-        if (answer < dp[triangle.size() - 1][i]) {
-            answer = dp[triangle.size() - 1][i];
+    for (int i = 0; i < n; i++) {
+        if (ans < dp[n - 1][i]) {
+            ans = dp[n - 1][i];
         }
     }
-    return answer;
+    
+    return ans;
 }
